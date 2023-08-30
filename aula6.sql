70cf2cba610ec8b00540473905c1b412e6883dbc
André Lucas <65183202+andreluke@users.noreply.github.com>
2023-08-29 22:54:05 -0300

    Update aula6.sql

--- aula6.sql
+++ aula6.sql
@@ -2,3 +2,7 @@ select a.titulo, b.status from tbl_titulo a inner join tbl_livros b ON a.codigo_
 
 select a.titulo, b.status, a.descricao from tbl_titulo a inner join tbl_livros b ON a.codigo_titulo = b.codigo_titulo
 where status='ALUGADO';
+
+select a.nome, c.status from tbl_cliente a inner join tbl_emprestimo b ON a.codigo_cliente = b.codigo_cliente
+inner join tbl_livros c ON b.codigo_cliente = c.cod_livro
+where status='DISPONIVEL';
